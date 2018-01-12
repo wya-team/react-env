5f6cd57a41df1b9e0522329383d25177aaeb626b
Deot <zrd0921@qq.com>
2018-01-12 15:51:27 +0800

    add wya-fetch wya-rc

--- src/pages/utils/net.js
+++ src/pages/utils/net.js
@@ -6,58 +6,20 @@
  * @param  {Object} options 扩展
  * 
  */
-// import { Toast } from 'antd-mobile';
-const ajax = (url, method, body = {}, localData, options = { noLoading: false }) => {
-	// url = url.includes(`?`) ? `${url}&` : `${url}?`
-	let isOk = false;
-	// let hasCanceled = false;
-	const { noLoading } = options;
-	// !noLoading && Toast.loading(null, 0);
-	return new Promise((resolve, reject) => {
-		const params = {
-			method,
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
-			},
-			body: Object.keys(body).length == 0 ? undefined : body
-		};
-		fetch(url, params)
-			.then((response) => {
-				// !noLoading && Toast.hide();
-				response.ok ? (isOk = true) : (isOk = false);
-				return response.json();
-			})
-			.then((responseData) => {
-				if (isOk) {
-					return responseData;
-				} else {
-					reject(responseData);
-				}
-			})
-			.then((responseData) => {
-				switch (responseData.status) {
-					case 1:
-						resolve(responseData);
-						break;
-					case 0:
-						reject(responseData);
-						break;
-					default :
-						reject({
-							msg: "数据格式不对"
-						});
-						break;
-				}
-			})
-			.catch((error) => {
-				// !noLoading && Toast.hide();
-				console.log(error, __DEV__);
-				reject({
-					msg: `返回非200${__DEV__ ? `，尝试开启模拟数据` : ``}`
-				});
-			});
-	});
+import { ajaxFn } from 'wya-fetch';
+const loadingFn = () => {
+
 };
+const loadedFn = () => {
+
+};
+const othersCb = () => {
+
+};
+const opts = {
+	
+};
+const ajax = ajaxFn(loadingFn, loadedFn, othersCb, opts);
 let net = {
 	ajax
 };
