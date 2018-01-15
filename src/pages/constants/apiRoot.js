8b56aa713d60fc56697088c21a708895cd85f7eb
Deot <zrd0921@qq.com>
2018-01-15 22:56:03 +0800

    fixed
    XMLHTTPRequest 取代 fetch，做跨域

--- src/pages/constants/apiRoot.js
+++ src/pages/constants/apiRoot.js
@@ -32,11 +32,11 @@ if (__DEV__) {
 	if (!DEV_WITH_SERVER) { // 开发环境-前端自模拟
 		baseUrl = 'http://localhost:3000/api';
 	} else { // 开发环境-后端数据
-		baseUrl = `${location.orgin}/api`;
+		baseUrl = `${location.origin}/api`;
 	}
 } else {
 	// 生产环境
-	baseUrl = `${location.orgin}`;
+	baseUrl = `${location.origin}`;
 }
 for (let i in API) {
 	API[i] = baseUrl + API[i];
--- src/pages/utils/net.js
+++ src/pages/utils/net.js
@@ -1,61 +1,201 @@
+const loadingFn = (msg) => {
+};
+const loadedFn = () => {
+};
+const setCb = () => {
+
+};
+const otherCb = () => {
+
+};
+const opts = {
+
+};
 /**
  * ajax
- * @param  {[type]} url     地址
- * @param  {[type]} method  请求类型
- * @param  {[type]} body    请求的参数
- * @param  {Object} options 扩展
- * 
+ * @param  {Func} loadingFn 加载时回调
+ * @param  {Func} loadedFn  结束时回调
+ * @param  {Func} setCb     结束时且200状态回调，可以设置配置信息，如果返回true，则终止
+ * @param  {Func} otherCb   结束时且200状态回调，status其他以外的值回调
+ * @param  {Object} opts   	参数
+ * @return {type}
  */
-// import { Toast } from 'antd-mobile';
-const ajax = (url, method, body = {}, localData, options = { noLoading: false }) => {
-	// url = url.includes(`?`) ? `${url}&` : `${url}?`
-	let isOk = false;
-	// let hasCanceled = false;
-	const { noLoading } = options;
-	// !noLoading && Toast.loading(null, 0);
-	return new Promise((resolve, reject) => {
-		const params = {
-			method,
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
-			},
-			body: Object.keys(body).length == 0 ? undefined : body
-		};
-		fetch(url, params)
-			.then((response) => {
-				// !noLoading && Toast.hide();
-				response.ok ? (isOk = true) : (isOk = false);
-				return response.json();
-			})
-			.then((responseData) => {
-				if (isOk) {
-					return responseData;
-				} else {
-					reject(responseData);
-				}
-			})
-			.then((responseData) => {
-				switch (responseData.status) {
-					case 1:
-						resolve(responseData);
-						break;
-					case 0:
-						reject(responseData);
-						break;
-					default :
-						reject({
-							msg: "数据格式不对"
-						});
-						break;
-				}
-			})
-			.catch((error) => {
-				// !noLoading && Toast.hide();
-				console.log(error, __DEV__);
-				reject({
-					msg: `返回非200${__DEV__ ? `，尝试开启模拟数据` : ``}`
-				});
+const HotPromise = Promise;
+const ajax = _opts => {
+	let xhr;
+	HotPromise.prototype.cancel = () => {
+		xhr instanceof XMLHttpRequest && (
+			xhr.abort(), 
+			xhr = null, 
+			console.log(`XMLHttpRequest Abort`)
+		);
+	};
+	return new HotPromise((resolve, reject) => {
+		/**
+		 * @param  {String} url 服务地址
+		 * @param  {Object} param 参数
+		 * @param  {Object} type 请求类型
+		 * @param  {Func} uploadProgress 上传回调
+		 * @param  {Bool} noLoading 不执行loadFn
+		 * @param  {Str} requestType 请求类型 'json' | 'form-data'
+		 * @param  {Str} tipMsg 提示文字
+		 */
+		let {
+			url,
+			param,
+			type = 'GET',
+			localData,
+			uploadProgress,
+			noLoading = false,
+			requestType,
+			tipMsg
+		} = _opts;
+		let method = type.toUpperCase(); // 默认转化为大写
+		if (!url && !localData) {
+			console.error('请求地址不存在');
+			reject({
+				msg: `开发时提示~参数错误`
 			});
+			return;
+		}
+		!noLoading && loadingFn && loadingFn(tipMsg);
+		let cgiSt = Date.now();
+		let onDataReturn = response => {
+			if (setCb) {
+				let isExit = setCb(response);
+				if (isExit) return;
+			}
+			// 正常流程
+			switch (response.status) {
+				case 1:
+				case true:
+					resolve(response);
+					return;
+				case 0:
+				case false:
+					reject(response);
+					return;
+				default:
+					otherCb && otherCb(response, resolve, reject);
+			}
+		};
+
+		/**
+		 * 如果本地已经从别的地方获取到数据，就不用请求了
+		 */
+		if (localData) {
+			!noLoading && loadedFn && loadedFn();
+			onDataReturn(localData);
+			return;
+		}
+		/**
+		 * 创建服务
+		 */
+		xhr = new XMLHttpRequest();
+		try {
+			xhr.onreadystatechange = () => {
+				if (xhr.readyState == 4) {
+
+					!noLoading && loadedFn && loadedFn(noLoading);
+					if (xhr.status >= 200 && xhr.status < 300) {
+						// 可以加上try-catch
+						try {
+							let data = JSON.parse(xhr.responseText);
+							onDataReturn(data);
+						} catch (e) {
+							let msg = "网络不稳定，请稍后重试.";
+							reject({
+								retcode: xhr.status,
+								msg: msg
+							});
+						}
+					} else {
+						if (xhr.status === 0 ){
+							// 主动取消
+							return;
+						}
+						reject({
+							retcode: xhr.status,
+							msg: '网络不稳定，请稍后重试..'
+						});
+					}
+					xhr = null;
+				}
+			};
+
+			let paramArray = [],
+				paramString = '';
+			for (let key in param) {
+				/**
+						 * 过滤掉值为null,undefined,''情况
+						 */
+				if (param[key] || param[key] === false || param[key] === 0) {
+					paramArray.push(key + '=' + encodeURIComponent(param[key]));
+				}
+			}
+
+			if (method === 'FORM') {
+				let formData = new FormData();　　　　
+				formData.append('file', param['file']);　　　　
+				formData.append('bkn', bkn);
+				xhr.upload.onprogress = (e) => {
+					if (e.lengthComputable) {
+						uploadProgress(e.loaded, e.total);
+					}
+				};
+				xhr.open('POST', url);
+				xhr.withCredentials = true;　　　　
+				xhr.send(formData);
+			} else if (method === 'JSONP') {
+				method = 'GET';
+
+				if (!param['callback']) {
+					reject({
+						status: 0
+					});
+				}
+
+				window[param['callback']] = (data) => {
+					onDataReturn(data);
+				};
+				if (paramArray.length > 0) {
+					url += (url.indexOf('?') > -1 ? '&' : '?') + paramArray.join('&');
+				}
+				let script = document.createElement("script");
+				let head = document.getElementsByTagName("head")[0];
+				script.src = url;
+				head.appendChild(script);
+			} else {
+				let req = '';
+				switch (method){
+					case 'PUT':
+					case 'POST':
+						req = JSON.stringify(param);
+						break;
+					case 'DELETE':
+					case 'GET':
+						if (paramArray.length > 0) {
+							url += (url.indexOf('?') > -1 ? '&' : '?') + paramArray.join('&');
+						}
+						break;
+					default:
+						break;
+				}
+				xhr.open(method, url, true);
+				xhr.withCredentials = true; // 允许发送cookie
+				// 跨域资源请求会发生两次 一次是204 可以参考cors // 无视就好
+				xhr.setRequestHeader(
+					'Content-Type', requestType == 'json' ? `application/json;charset=utf-8` : `application/x-www-form-urlencoded`
+				);
+				xhr.setRequestHeader(
+					'X-Requested-With', 'XMLHttpRequest'
+				);
+				xhr.send(method === 'POST' ? paramArray.join('&') : '');
+			}
+
+		} catch (e) {
+			console.error(e);
+		}
 	});
 };
 let net = {
