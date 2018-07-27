19a255379a438ac3f51ba6c61870d9e4f44bac5b
Deot <zrd0921@qq.com>
2018-07-27 11:13:22 +0800

    update

--- src/pages/containers/Home/App.js
+++ src/pages/containers/Home/App.js
@@ -1,9 +1,9 @@
 import { redirectUserToLogin, redirectUserToHome } from '@router/auth';
-import Layout from './Layout/Layout';
+// import Layout from './Layout/Layout';
 export const homeConfig = [
 	{
 		path: '/home',
-		component: Layout,
+		// component: Layout, // 移动端可以去掉，PC端使用
 		childRoutes: [
 			{
 				path: 'main',
