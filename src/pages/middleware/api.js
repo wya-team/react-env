bbc7300fceea7db95ff247b967f4c87315ceb3c3
Deot <zrd0921@qq.com>
2018-08-23 02:27:26 +0800

    fix `restful` param(del id)

--- src/pages/middleware/api.js
+++ src/pages/middleware/api.js
@@ -13,7 +13,7 @@ const middlewareApi = store => next => action => {
 	 * 如果有传递localData，就不会触发ajax了，直接触发_success
 	 * 当前也可以传其他参数
 	 */
-	let { 
+	let {
 		localData,
 		requestOnStop, // 是否需要出发请求_ON
 		setPage,
@@ -53,8 +53,8 @@ const middlewareApi = store => next => action => {
 	result = net.ajax({
 		url: API_ROOT[apiName],
 		type: ajaxType,
-		param,
-		localData, 
+		param: { ...param },
+		localData,
 		noLoading: param.page === undefined ? noLoading : true
 	}).then(data => {
 		params = { // 由于后端格式是status:1,data:{}
@@ -62,7 +62,7 @@ const middlewareApi = store => next => action => {
 			data: data.data
 		};
 		//  触发请求成功的action
-		pullToRefresh 
+		pullToRefresh
 			? next(nextAction(`${redirectActionType || apiName}_REFRESH`, params, opts))
 			: next(nextAction(`${redirectActionType || apiName}_SUCCESS`, params, opts));
 		onSuccess && onSuccess(data);
